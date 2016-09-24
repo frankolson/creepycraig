e6b3c2542f1b1a239d1aecc6eabfd32ac75a18ce
Frank Olson <will.olson11@gmail.com>
2016-09-24 12:02:42 -0700

    added price filters back in, fixed coordinate loop, removed typo, added price to output

--- scraper.py
+++ scraper.py
@@ -7,16 +7,17 @@ import settings
 import util
 
 for area in settings.AREAS:
-    print "searching " + area
     cl = CraigslistHousing( site=settings.SITE, area=area, category='apa',
-                            )
+                            filters={
+                                'max_price': settings.MAX_PRICE,
+                                'min_price': settings.MIN_PRICE
+                            })
 
     results = cl.get_results(sort_by='newest', geotagged=True, limit=20)
 
     for result in results:
         geotag = result["geotag"]
-        print geotag
         if geotag != None:
-            for coords in settings.BOXES.items():
+            for a, coords in settings.BOXES.items():
                 if util.in_box(geotag, coords):
-                    print result["name"]
+                    print a + "("+ result["price"] +"): " + result["name"]
